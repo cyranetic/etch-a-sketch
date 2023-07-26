9c8d733991d5c6d1a5d224817e76ed8e0903dc72
Sara Segel <sara.segel@gmail.com>
2023-07-26 22:20:52 +0900

    add resize button and functionality

--- scripts.js
+++ scripts.js
@@ -1,25 +1,48 @@
 const container = document.querySelector("body");
 
+//default number of squares per side
+let squares = 16;
+
+//add title
 const title = document.createElement("h1");
 title.textContent = "Etch-A-Sketch";
 container.appendChild(title);
 
-const gameContainer = document.createElement("div");
-gameContainer.classList.add("game-container");
-container.appendChild(gameContainer);
+//Add a button to the top of the screen that will send the user a popup asking for the number of squares per side for the new grid. Once entered, the existing grid should be removed and a new grid should be generated in the same total space as before (e.g. 960px wide) so that you’ve got a new sketch pad.
 
-//make a 16 x 16 grid of these divs:
-for (let i = 0; i < 16; i++) {
-  for (let i = 0; i < 16; i++) {
-    const pixel = document.createElement("div");
-    pixel.addEventListener("mouseenter", (e) => {
-      console.log(e.target);
-      e.target.style.backgroundColor = "pink";
-    });
-    gameContainer.appendChild(pixel);
+const squaresButton = document.createElement("button");
+squaresButton.textContent = "Resize";
+container.appendChild(squaresButton);
+
+//make a square grid containing the given number of squares:
+function createGrid() {
+  for (let i = 0; i < squares; i++) {
+    for (let i = 0; i < squares; i++) {
+      const pixel = document.createElement("div");
+      pixel.classList.add("pixel");
+      pixel.style.flex = `0 0 ${(1 / squares) * 100}%`;
+      //make color change on hover on the div squares
+      pixel.addEventListener("mouseenter", (e) => {
+        console.log(e.target);
+        e.target.style.backgroundColor = "pink";
+      });
+      gameContainer.appendChild(pixel);
+    }
   }
 }
 
-//make color change on hover on the div squares
+//Initialize the Etch-a-Sketch: create frame and fill with default number of squares
+const gameContainer = document.createElement("div");
+gameContainer.classList.add("game-container");
+container.appendChild(gameContainer);
+createGrid();
 
-//Add a button to the top of the screen that will send the user a popup asking for the number of squares per side for the new grid. Once entered, the existing grid should be removed and a new grid should be generated in the same total space as before (e.g. 960px wide) so that you’ve got a new sketch pad. Tip: Set the limit for the user input to a maximum of 100. A larger number of squares results in more computer resources being used, potentially causing delays, freezing, or crashing that we want to prevent.
+squaresButton.addEventListener("click", (e) => {
+  squares = prompt("How many squares per side? Let's keep it under 100");
+  if (squares > 100) {
+    squares = prompt("How many squares per side? Let's keep it under 100");
+  } else if (squares <= 100) {
+    gameContainer.replaceChildren();
+    createGrid();
+  }
+});
